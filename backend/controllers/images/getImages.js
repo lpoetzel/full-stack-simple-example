84a1b90a8da7e3368a5475de51d90875f26dbebf
Lars Poetzel <lars.poetzel@gmail.com>
2022-12-08 16:46:49 +0100

    done

--- backend/controllers/images/getImages.js
+++ backend/controllers/images/getImages.js
@@ -7,7 +7,7 @@ const getImages = async (req, res, next) => {
   // write a query that returns values from multiple tables
   // hint : you have to use JOIN
   const query =
-    "select images.image_id,images.title, images.url, products.title as product, categories.title AS category from images left join products on products.product_id = images.product_id left join categories on categories.category_id = images.category_id";
+    "select images.image_id,images.title, images.url as image, products.title as product, categories.title AS category from images left join products on products.product_id = images.product_id left join categories on categories.category_id = images.category_id";
   connection
     .promise()
     .query(query)
